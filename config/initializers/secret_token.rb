2e94c7e96a626633e58f931fb7296a9814bb7d66
Devi Firdaus <dfedogawa3@gmail.com>
2013-08-05 22:12:01 +0700

    first start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppMutiah::Application.config.secret_token = 'd239a291032a6fae39b41c0644adf2fdf133085ddb82abd88edbd557688d69356b3f93643941cae89e6ea15c65e40028947e564f2da2617e95a1766af3a2dbca'
